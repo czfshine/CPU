3bcea1e4e253f7e049dd33795d656751b00d1dcb
czfshine <czfshine@outlook.com>
2018-05-30 13:26:35 +0800

    增加进位选择器

--- t1.vhd
+++ t1.vhd
@@ -9,4 +9,17 @@ end t1;
 
 architecture behave of t1 is
 begin
+	process(sci,flag_c)
+	begin
+		case sci is
+			when "00"=>
+				alu_cin<='0';
+			when "01"=>
+				alu_cin<='1';
+			when "10"=>
+				alu_cin<=flag_c;
+			when others=>
+				alu_cin<='0';
+			end case;
+	end process;
 end behave;
