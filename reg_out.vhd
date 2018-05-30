54e60ab4cd750f32e626ae8c7008463e6903b810
czfshine <czfshine@outlook.com>
2018-05-30 22:01:07 +0800

    寄存器输出

--- reg_out.vhd
+++ reg_out.vhd
@@ -5,7 +5,7 @@ entity reg_out is
     port(ir,pc,reg_in:        in std_logic_vector(7 downto 0);
          offset,alu_a,alu_b:  in std_logic_vector(7 downto 0);
          alu_out,reg_testa:   in std_logic_vector(7 downto 0);
-         reg_sel:             in std_logic_vector(3 downto 0);
+         reg_sel:             in std_logic_vector(1 downto 0);
          sel:                 in std_logic_vector(1 downto 0);
          reg_data:            out std_logic_vector(7 downto 0));
 end reg_out;
