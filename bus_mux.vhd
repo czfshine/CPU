c9d861e1970d59ce2696677b9c36ef42162d0f91
czfshine <czfshine@outlook.com>
2018-05-30 22:00:30 +0800

    数据总线控制器

--- bus_mux.vhd
+++ bus_mux.vhd
@@ -9,4 +9,30 @@ end bus_mux;
 
 architecture behave of bus_mux is
 begin
+	process(alu_in_sel,data,pc,offset,sr,dr)
+	begin
+		case alu_in_sel is
+		when "000"=>
+		alu_sr<=sr;
+		alu_dr<=dr;
+		when "001"=>
+		alu_sr<=sr;
+		alu_dr<="00000000";
+		when "010"=>
+		alu_sr<="00000000";
+		alu_dr<=dr;
+		when "011"=>
+		alu_sr<=offset;
+		alu_dr<=pc;
+		when "100"=>
+		alu_sr<="00000000";
+		alu_dr<=pc;
+		when "101"=>
+		alu_sr<="00000000";
+		alu_dr<=data;
+		when others=>
+		alu_sr<="00000000";
+		alu_dr<="00000000";
+		end case;
+	end process;
 end behave;
